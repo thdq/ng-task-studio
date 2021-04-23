0965e3deaa4e7b8dd08ce970cdef436fabec3422
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-04-23 20:33:36 -0300

    feat: ensure TaskList throws if HttpPostClient returns 400 on UnexpectedError

--- src/data/usecases/tasks-lists/task-list.ts
+++ src/data/usecases/tasks-lists/task-list.ts
@@ -1,4 +1,6 @@
+import { UnexpectedError } from "@/data/errors/unexpected"
 import { HttpPostClient } from "@/data/protocols/http/http-post-client"
+import { HttpStatusCode } from "@/data/protocols/http/http-response"
 import { TaskListModel } from "@/domain/models/task-list"
 import { CreateTaskList, TaskListParams } from "@/domain/usecases/tasks-lists/create-task-list"
 
@@ -16,9 +18,12 @@ export class TaskList implements CreateTaskList {
         const httpResponse = await this.httpPostClient.post({
             url: this.url,
             body: params
-        })        
+        })
         
-        return httpResponse.body
+        switch (httpResponse.statusCode) {
+            case HttpStatusCode.success: return httpResponse.body
+            default: throw new UnexpectedError()
+        }        
         
     }
     
