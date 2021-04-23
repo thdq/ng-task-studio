5a9dac2d5a012c3bb1666c601114a66d57dfd035
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-04-23 20:21:04 -0300

    test: ensure TaskList calls HttpPostClient with correct URL

---
+++ src/data/usecases/tasks-lists/task-list.spec.ts
@@ -0,0 +1,67 @@
+import { HttpPostParams } from '@/data/protocols/http/http-params'
+import { HttpPostClient } from '@/data/protocols/http/http-post-client'
+import { HttpResponse } from '@/data/protocols/http/http-response'
+import { TaskListModel } from '@/domain/models/task-list'
+import { TaskListParams } from '@/domain/usecases/tasks-lists/create-task-list'
+import faker from 'faker'
+import { TaskList } from './task-list'
+
+interface SutType {
+    sut: TaskList
+    httpPostClientStub: HttpPostClient<TaskListParams, TaskListModel>
+}
+
+const makeHttpPostClient = (): HttpPostClient<TaskListParams, TaskListModel> => {
+
+    class HttpPostClientStub<T, R> implements HttpPostClient<T, R> {
+        url?: string
+        body?: T
+        response: HttpResponse<R> = {
+            statusCode: 200
+        }
+
+        async post (params: HttpPostParams<T>): Promise<HttpResponse<R>> {
+
+            const { url, body } = params
+
+            this.url = url
+            this.body = body
+            return await Promise.resolve(this.response)
+        }
+    }
+
+    return new HttpPostClientStub()
+
+}
+
+const makeSut = (url: string = faker.internet.url()): SutType => {
+    
+    const httpPostClientStub = makeHttpPostClient()
+    const sut = new TaskList(url, httpPostClientStub)
+    
+    return {
+        sut,
+        httpPostClientStub
+    }
+    
+}
+
+describe('TaskList use case', () => {
+
+    test('Should call HttpPostClient with correct URL', async () => {
+
+        const url = faker.internet.url()
+        
+        const { sut, httpPostClientStub } = makeSut(url)
+        
+        const taskListParams: TaskListParams = {
+            title: faker.random.words()
+        }
+        
+        await sut.create(taskListParams)
+        
+        expect(httpPostClientStub.url).toBe(url)
+
+    })
+
+})
