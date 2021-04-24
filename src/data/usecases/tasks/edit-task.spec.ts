81d9ac25ed4dd62953f1b1c9d1ebbb9a3acf2b37
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-04-24 01:43:36 -0300

    test: ensure EditTask calls HttpPutClient with correct URL

---
+++ src/data/usecases/tasks/edit-task.spec.ts
@@ -0,0 +1,69 @@
+import { HttpPutParams } from '@/data/protocols/http/http-params'
+import { HttpPutClient } from '@/data/protocols/http/http-put-client'
+import { HttpResponse, HttpStatusCode } from '@/data/protocols/http/http-response'
+import { TaskModel } from '@/domain/models/task'
+import { TaskParams } from '@/domain/usecases/task'
+import faker from 'faker'
+import { EditTask } from './edit-task'
+
+interface SutTypes {
+    sut: EditTask
+    httpPutClientStub: HttpPutClient<TaskParams, TaskModel>
+}
+
+const makeHttpPutClient = (): HttpPutClient<TaskParams, TaskModel> => {
+
+    class HttpPutClientStub<T, R> implements HttpPutClient<T, R> {
+        url?: string
+        body?: T
+        response: HttpResponse<R> = {
+            statusCode: HttpStatusCode.success
+        }
+
+        async put (params: HttpPutParams<T>): Promise<HttpResponse<R>> {
+
+            const { url, body } = params
+
+            this.url = url
+            this.body = body
+            return await Promise.resolve(this.response)
+        }
+    }
+
+    return new HttpPutClientStub()
+
+}
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+    
+    const httpPutClientStub = makeHttpPutClient()
+    const sut = new EditTask(url, httpPutClientStub)
+    
+    return {
+        sut,
+        httpPutClientStub
+    }
+    
+}
+
+describe('EditTask use case', () => {
+
+    test('Should call HttpPutClient with correct URL', async () => {
+
+        const url = faker.internet.url()
+            
+        const { sut, httpPutClientStub } = makeSut(url)
+            
+        const taskParams: TaskParams = {
+            title: faker.random.words(),
+            completed: false,
+            listId: faker.datatype.number()
+        }
+            
+        await sut.update(taskParams)
+            
+        expect(httpPutClientStub.url).toBe(url)
+    
+    })
+
+})
