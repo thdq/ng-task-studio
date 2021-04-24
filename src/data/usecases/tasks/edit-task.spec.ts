cc9d54f389ad4f8f1f1eb8c7ecfcbbcbacc1b4e8
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-04-24 01:53:10 -0300

    test: ensure EditTask throws if HttpPutClient returns 40X/50X on UnexpectedError

--- src/data/usecases/tasks/edit-task.spec.ts
+++ src/data/usecases/tasks/edit-task.spec.ts
@@ -1,4 +1,5 @@
 import { MissingParamsError } from '@/data/errors/missing-params-error'
+import { UnexpectedError } from '@/data/errors/unexpected'
 import { HttpPutParams } from '@/data/protocols/http/http-params'
 import { HttpPutClient } from '@/data/protocols/http/http-put-client'
 import { HttpResponse, HttpStatusCode } from '@/data/protocols/http/http-response'
@@ -136,6 +137,66 @@ describe('EditTask use case', () => {
 
         await expect(promise).rejects.toThrow(new MissingParamsError("title"))
         
+    })
+    
+    test('Should throw if HttpPutClient returns 400 on UnexpectedError', async () => {
+
+        const { sut, httpPutClientStub } = makeSut()
+
+        httpPutClientStub.response = {
+            statusCode: HttpStatusCode.badRequest
+        }
+
+        const taskParams: TaskParams = {
+            title: faker.random.words(),
+            completed: false,
+            listId: faker.datatype.number()
+        }
+
+        const promise = sut.update(taskParams)
+
+        await expect(promise).rejects.toThrow(new UnexpectedError())
+
+    })
+    
+    test('Should throw if HttpPutClient returns 404 on UnexpectedError', async () => {
+
+        const { sut, httpPutClientStub } = makeSut()
+
+        httpPutClientStub.response = {
+            statusCode: HttpStatusCode.notFound
+        }
+
+        const taskParams: TaskParams = {
+            title: faker.random.words(),
+            completed: false,
+            listId: faker.datatype.number()
+        }
+
+        const promise = sut.update(taskParams)
+
+        await expect(promise).rejects.toThrow(new UnexpectedError())
+
+    })
+    
+    test('Should throw if HttpPutClient returns 500 on UnexpectedError', async () => {
+
+        const { sut, httpPutClientStub } = makeSut()
+
+        httpPutClientStub.response = {
+            statusCode: HttpStatusCode.serverError
+        }
+
+        const taskParams: TaskParams = {
+            title: faker.random.words(),
+            completed: false,
+            listId: faker.datatype.number()
+        }
+
+        const promise = sut.update(taskParams)
+
+        await expect(promise).rejects.toThrow(new UnexpectedError())
+
     })    
 
 })
