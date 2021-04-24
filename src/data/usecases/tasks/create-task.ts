947fd8528cc0e1be24a3e16ecd3a253257200ad4
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-04-24 01:24:18 -0300

    feat: ensure CreateTask calls Validation with correct value

--- src/data/usecases/tasks/create-task.ts
+++ src/data/usecases/tasks/create-task.ts
@@ -1,3 +1,4 @@
+import { MissingParamsError } from "@/data/errors/missing-params-error"
 import { UnexpectedError } from "@/data/errors/unexpected"
 import { HttpPostClient } from "@/data/protocols/http/http-post-client"
 import { HttpStatusCode } from "@/data/protocols/http/http-response"
@@ -10,12 +11,17 @@ export class CreateTask implements Task {
     private readonly httpPostClient: HttpPostClient<TaskParams, TaskModel>
     private readonly validation: Validation
     
-    constructor (url: string, httpPostClient: HttpPostClient<TaskParams, TaskModel>) {
+    constructor (url: string, httpPostClient: HttpPostClient<TaskParams, TaskModel>, validation: Validation) {
         this.url = url
         this.httpPostClient = httpPostClient
+        this.validation = validation
     }
     
     async create (params: TaskParams): Promise<TaskModel> {
+        
+        const validation = this.validation.validate(params)
+        
+        if (validation.error) throw new MissingParamsError(validation.failedField)
                 
         const httpResponse = await this.httpPostClient.post({
             url: this.url,
