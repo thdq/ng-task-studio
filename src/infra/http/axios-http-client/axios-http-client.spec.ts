938f05d91af080b17f725a596de3f8cdfc8c77db
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-04-24 12:34:18 -0300

    test: ensure AxiosHttpClient calls axios with correct values

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,58 @@
+import { HttpRequest } from '@/data/protocols/http/http-request'
+import axios from 'axios'
+import faker from 'faker'
+import { AxiosHttpClient } from './axios-http-client'
+
+jest.mock('axios')
+
+interface SutTypes {
+    sut: AxiosHttpClient
+    mockedAxios: jest.Mocked<typeof axios>
+}
+
+const makeAxios = (): jest.Mocked<typeof axios> => {
+    
+    const mockHttpResponse = (): any => ({
+        data: faker.random.objectElement(),
+        status: faker.datatype.number()
+    })
+    
+    const mockedAxios = axios as jest.Mocked<typeof axios>
+    
+    mockedAxios.request.mockClear().mockResolvedValue(mockHttpResponse())
+    
+    return mockedAxios
+}
+
+const makeSut = (): SutTypes => {
+    const sut = new AxiosHttpClient()
+    const mockedAxios = makeAxios()
+    return {
+        sut,
+        mockedAxios
+    }
+}
+
+describe('AxiosHttpClient', () => {
+    test('Should call axios with correct values', async () => {
+        
+        const request: HttpRequest = {
+            url: faker.internet.url(),
+            method: faker.random.arrayElement(['get', 'post', 'put', 'delete']),
+            body: faker.random.objectElement(),
+            headers: faker.random.objectElement()
+        }
+          
+        const { sut, mockedAxios } = makeSut()
+
+        await sut.request(request)
+
+        expect(mockedAxios.request).toHaveBeenCalledWith({
+            url: request.url,
+            data: request.body,
+            headers: request.headers,
+            method: request.method
+        })
+    })
+
+})
