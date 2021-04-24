e7548d665a7814171aaeb4dfe21766e29fda67b8
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-04-23 23:08:31 -0300

    feat: ensure EditTaskList calls Validation with correct value

--- src/data/usecases/tasks-lists/edit-task-list.ts
+++ src/data/usecases/tasks-lists/edit-task-list.ts
@@ -3,17 +3,22 @@ import { HttpPutClient } from "@/data/protocols/http/http-put-client"
 import { HttpStatusCode } from "@/data/protocols/http/http-response"
 import { TaskListModel } from "@/domain/models/task-list"
 import { TaskList, TaskListParams } from "@/domain/usecases/tasks-lists/create-task-list"
+import { Validation } from "@/validation/validation"
 
 export class EditTaskList implements TaskList {
     private readonly url: string
     private readonly httpPutClient: HttpPutClient<TaskListParams, TaskListModel>
+    private readonly validation: Validation
     
-    constructor (url: string, httpPutClient: HttpPutClient<TaskListParams, TaskListModel>) {
+    constructor (url: string, httpPutClient: HttpPutClient<TaskListParams, TaskListModel>, validation: Validation) {
         this.url = url
         this.httpPutClient = httpPutClient
+        this.validation = validation
     }
     
     async update (params: TaskListParams): Promise<TaskListModel> {
+        
+        this.validation.validate(params)
         
         const httpResponse = await this.httpPutClient.put({
             url: this.url,
