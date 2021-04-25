9682e7ef15f27480fd0299076e009ce21573ae09
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-04-25 16:56:22 -0300

    chore: configure angular jest preset

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,25 @@
 module.exports = {
+    "preset": "jest-preset-angular",
+    "setupFilesAfterEnv": [
+        "<rootDir>/setupJest.ts"
+    ],
+    "testPathIgnorePatterns": [
+        "<rootDir>/node_modules/",
+        "<rootDir>/dist/",
+        "<rootDir>/src/test.ts"
+    ],
+    "globals": {
+        "ts-jest": {
+            "tsConfig": "<rootDir>/tsconfig.spec.json",
+            "stringifyContentPathRegex": "\\.html$",
+            "astTransformers": {
+                "before": [
+                    "jest-preset-angular/build/InlineFilesTransformer",
+                    "jest-preset-angular/build/StripStylesTransformer"
+                ]
+            }
+        }
+    },
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
         '^~/(.*)$': '<rootDir>/src/presentation/$1',
@@ -7,7 +28,7 @@ module.exports = {
         'ts',
         'js',
         'json'
-    ],        
+    ],
     collectCoverageFrom: [
         '<rootDir>/src/**/*.ts',
         '!<rootDir>/src/main/**'
