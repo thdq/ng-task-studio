00d0c282a7e2378104c266a9646c31f031765783
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-04-25 17:44:42 -0300

    feat: add MyTodoComponent layout

---
+++ src/app/layouts/my-todo/my-todo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'my-todo',
+    templateUrl: './my-todo.component.html',
+    styleUrls: ['./my-todo.component.scss']
+})
+
+export class MyTodoComponent {
+
+}
